517f520049cf1bfa794912bc38c55f8ac237547d
MCNGN <rifqi.adibbahagia@binus.ac.id>
2026-01-25 21:11:05 +0700

    Update Process Z-Score

--- package/process/process.py
+++ package/process/process.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+from sklearn.cluster import KMeans
 
 project_root = Path(__file__).resolve().parents[2]
 processed_dir = project_root / "processed_data"
@@ -12,4 +13,11 @@ def process_z_score(filename):
     for column in columns:
         df[f'{column}_z_score'] = df.groupby('year')[column].transform( lambda x : (x - x.mean()) / x.std())
 
-    df.to_csv(processed_dir / "merge_data_zscore.csv", index=False)
+    processed_file = processed_dir / "merge_data_zscore.csv"
+
+    df.to_csv(processed_file, index=False)
+
+    return processed_file
+
+def process_clustering():
+    print("Hello World !")
